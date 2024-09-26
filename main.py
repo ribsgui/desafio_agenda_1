fb1d7c7216f4181d0d02d6e6e9e09ed427c9bfa0
Guilherme Ribeiro <guiribeiro594@gmail.com>
2024-09-26 14:41:41 -0300

    Create Agenda class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def main():
 
+
 if __name__ == "__main__":
     main()
---
+++ models/Agenda.py
@@ -0,0 +1,35 @@
+from models.Contato import Contato
+
+class Agenda:
+    def __init__(self):
+        self.contatos = []
+    
+    def add_contacts(self, name, number, email):
+        contato = Contato(name, number, email)
+        self.contatos.append(contato)
+    
+    def list_contacts(self):
+        for contato in self.contatos:
+            print(contato)
+    
+    def edit_contacts(self, id, name, number, email):
+        if 0 <= id < len(self.contatos):
+            if name:
+                self.contatos[id].name = name
+            if number:
+                self.contatos[id].number = number
+            if email:
+                self.contatos[id].email = email
+    
+    def change_fav(self, id):
+        if 0 <= id < len(self.contatos):
+            contato.mark_fav()
+            
+    def list_fav(self):
+        favorites = [contato for contato in self.contatos if contato.fav]
+        for contato in favorites:
+            print(contato)
+            
+    def delete_contact(self, id):
+        if 0 <= id < len(self.contatos):
+            self.contatos.pop[id]
